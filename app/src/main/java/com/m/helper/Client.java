d65f950062e6a7f4681a17e438649117022d2d96
Michal Stokluska <mstoklus@redhat.com>
2019-11-13 09:01:33 +0000

    feat: adding cache

--- app/src/main/java/com/m/helper/Client.java
+++ app/src/main/java/com/m/helper/Client.java
@@ -1,7 +1,21 @@
 package com.m.helper;
 
+import android.content.Context;
+
 import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Operation;
+import com.apollographql.apollo.api.ResponseField;
+import com.apollographql.apollo.cache.normalized.CacheKey;
+import com.apollographql.apollo.cache.normalized.CacheKeyResolver;
+import com.apollographql.apollo.cache.normalized.NormalizedCacheFactory;
+import com.apollographql.apollo.cache.normalized.lru.EvictionPolicy;
+import com.apollographql.apollo.cache.normalized.lru.LruNormalizedCache;
+import com.apollographql.apollo.cache.normalized.lru.LruNormalizedCacheFactory;
+import com.apollographql.apollo.cache.normalized.sql.ApolloSqlHelper;
+import com.apollographql.apollo.cache.normalized.sql.SqlNormalizedCacheFactory;
 import com.apollographql.apollo.subscription.WebSocketSubscriptionTransport;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,7 +25,36 @@ import okhttp3.Request;
 
 public class Client {
 
-    public static ApolloClient setupApollo(String serverUrl, String authHeader) {
+    public static ApolloClient setupApollo(String serverUrl, String authHeader, Context context) {
+
+
+        CacheKeyResolver resolver = new CacheKeyResolver() {
+            @NotNull
+            @Override
+            public CacheKey fromFieldRecordSet(@NotNull ResponseField field, @NotNull Map<String, Object> recordSet) {
+                return formatCacheKey((String) recordSet.get("id"));
+            }
+
+            @NotNull
+            @Override
+            public CacheKey fromFieldArguments(@NotNull ResponseField field, @NotNull Operation.Variables variables) {
+                return formatCacheKey((String) field.resolveArgument("id", variables));
+            }
+
+            private CacheKey formatCacheKey(String id) {
+                if (id == null || id.isEmpty()) {
+                    return CacheKey.NO_KEY;
+                } else {
+                    return CacheKey.from(id);
+                }
+            }
+        };
+
+        String DB_CACHE_NAME = "myapp";
+        ApolloSqlHelper apolloSqlHelper = new ApolloSqlHelper(context, DB_CACHE_NAME);
+
+        NormalizedCacheFactory<LruNormalizedCache> cacheFactory = new LruNormalizedCacheFactory(EvictionPolicy.NO_EVICTION)
+                .chain(new SqlNormalizedCacheFactory(apolloSqlHelper));
 
         OkHttpClient okHttpClient = new OkHttpClient
                 .Builder()
@@ -27,11 +70,12 @@ public class Client {
         connectionParams.put("Authorization", authHeader);
 
         return ApolloClient.builder()
+                .enableAutoPersistedQueries(true)
                 .serverUrl(serverUrl)
+                .normalizedCache(cacheFactory, resolver)
                 .subscriptionConnectionParams(connectionParams)
                 .subscriptionTransportFactory(new WebSocketSubscriptionTransport.Factory(serverUrl, okHttpClient))
                 .okHttpClient(okHttpClient)
                 .build();
-
     }
 }
